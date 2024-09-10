d03ec7b2640fd2d80631b6b8b94697e3c789b621
NorDar <44843808+NorDar@users.noreply.github.com>
2024-09-10 14:54:02 +0000

    added dynamic paths in container creation

--- create-container.sh
+++ create-container.sh
@@ -7,19 +7,19 @@ done
 echo "starting container with jupyter notebook on port $port"
 
 
-USER_NAME=id -u
-jupyter_command='export PATH=$PATH:/.local/bin; jupyter notebook --no-browser --ip=0.0.0.0 --notebook-dir /home/$USER_NAME --NotebookApp.token=<YOUR PASSWORD> --port='"$port"
+USER_NAME=$(id -u -n)
+jupyter_command="export PATH=${PATH}:/.local/bin; jupyter notebook --no-browser --ip=0.0.0.0 --notebook-dir /home/${USER_NAME} --NotebookApp.token=<YOUR PASSWORD> --port=${port}"
 
 docker run -ti -d \
-        --name t-sne-feature-export-$USER_NAME \
+	--name "t-sne-feature-export-$USER_NAME" \
         -u "$(id -u)":"$(id -g)" \
-        -v /home/$USER_NAME:/home/$USER_NAME \
+	-v "/home/$USER_NAME:/home/$USER_NAME" \
         -v /home:/host-homes \
         -v /home:/tf/notebooks \
         -v /etc/group:/etc/group:ro \
         -v /etc/passwd:/etc/passwd:ro \
         -v /etc/shadow:/etc/shadow:ro \
-        --workdir=/home/$USER_NAME/t-sne-features \
+	--workdir="/home/$USER_NAME/t-sne-features" \
         -p "$port":"$port" \
         nordar/stroke_perfusion:2.4.0-publish \
         /bin/bash $jupyter_command
